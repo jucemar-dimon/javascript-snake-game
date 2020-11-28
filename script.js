cfb77f2b90da2b5e26c53cd580378dcdb6267bdf
Jucemar Dimon <jucemar.dimon@gmail.com>
2020-11-28 18:19:38 -0300

    Movimentos da cobrinha foram definidos

---
+++ script.js
@@ -0,0 +1,40 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = { x: 8 * box, y: 8 * box };
+let direction = "right";
+
+function criarRGB() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+function iniciarJogo() {
+  criarRGB();
+  criarCobrinha();
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  console.log(snake[0].x);
+
+  if (direction === "right") snakeX += box;
+  if (direction === "left") snakeX -= box;
+  if (direction === "up") snakeY -= box;
+  if (direction === "down") snakeY += box;
+
+  snake.pop();
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
